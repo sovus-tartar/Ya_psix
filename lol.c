f4f1b245e96182e005c21963aada57cf9e9632fa
Sovus_tartar <vad-nesmejanov@yandex.ru>
2021-11-11 21:28:07 +0300

    git pull

---
+++ lol.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct recept
+{
+    unsigned long long timestamp;
+    unsigned id;
+    unsigned recept_id;
+};
+
+int main()
+{
+    int n, i, temp;
+    struct recept *rec;
+    temp = 0;
+    scanf("%d", &n);
+    calloc
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%llu%u%u", rec.timestamp, rec.id, rec.recept_id);
+        
+    }
+
+    return 0;
+}
--- test/rect.c
+++ test/rect.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-int h; // часы
-int min; // минуты (от 0 до 59)
+typedef struct
+{
+    int h;   // часы
+    int min; // минуты (от 0 до 59)
 } TicTac;
 
-TicTac after(TicTac a, int min){
+TicTac after(TicTac a, int min)
+{
 
-TicTac c;
+    TicTac c;
 
-c.min = (a.min + min)%60;
-c.h = (a.h - 1 + ((a.min + min)/60))%12 + 1;
+    c.min = (a.min + min) % 60;
+    c.h = (a.h - 1 + ((a.min + min) / 60)) % 12 + 1;
 
-printf ("%d\n%d\n", c.h, c.min);
-return c;
-
+    printf("%d\n%d\n", c.h, c.min);
+    return c;
 }
 
-int main (){
-TicTac a, c;
-int min;
+int main()
+{
+    TicTac a, c;
+    int min;
 
-
-scanf ("%d %d", &(a.h), &(a.min));
-scanf ("%d", &min);
-c = after(a, min);
-printf ("%.2d:%.2d\n", c.h, c.min);
-return 0;
+    scanf("%d %d", &(a.h), &(a.min));
+    scanf("%d", &min);
+    c = after(a, min);
+    printf("%.2d:%.2d\n", c.h, c.min);
+    return 0;
 }
