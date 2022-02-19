fb69ea36222c1baff3069e4fe9104a817e5f5c2a
Sovus_tartar <vad-nesmejanov@yandex.ru>
2022-02-19 05:40:21 +0300

    master

---
+++ sem_5/CL/cl.c
@@ -0,0 +1,230 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+struct list_t
+{
+    struct list_t *next;
+    int data;
+};
+
+struct list_t *cl_push(struct list_t *pre, int val)
+{
+
+    struct list_t *temp;
+
+    temp = pre->next;
+
+    pre->next = calloc(1, sizeof(struct list_t));
+    assert(pre->next != 0);
+
+    pre->next->next = temp;
+    pre->next->data = val;
+
+    return (pre->next);
+}
+
+struct list_t *cl_pop(struct list_t *pre)
+{
+
+    struct list_t *temp;
+
+    temp = pre->next->next;
+
+    free(pre->next);
+
+    pre->next = temp;
+
+    return pre;
+}
+
+int minimum(int const *arr, int num)
+{
+    int i, min;
+    min = arr[0];
+    for (i = 1; i < num; i++)
+        if (arr[i] < min)
+            min = arr[i];
+    return min;
+}
+
+struct list_t *list_init(int n)
+{
+    int i;
+    struct list_t *top, *curr;
+
+    top = calloc(1, sizeof(struct list_t)); //1 time
+    curr = top;
+
+    for (i = 0; i < n - 1; i++)
+    { //n - 1 times
+        curr->next = calloc(1, sizeof(struct list_t));
+        curr = curr->next;
+    }
+    curr->next = top;
+
+    return top;
+}
+
+struct list_t *list_skip(struct list_t *node, int n)
+{
+    struct list_t *temp;
+    int i;
+
+    temp = node;
+    assert(temp != NULL);
+    for (i = 0; i < n; i++)
+    {
+        temp = temp->next;
+    }
+
+    return temp;
+}
+
+int coin_num(int *arr, int num, int sum)
+{
+    int number, i, j, k, p, min, pre, l;
+    int *temp_arr;
+    struct list_t *curr, *curr_top, *temp;
+    temp = NULL;
+    curr_top = calloc(1, sizeof(struct list_t));
+    j = 0;
+    curr_top->next = curr_top;
+    //display_list(curr_top, 1);
+
+    for (p = 1; p <= sum; p++)
+    {
+        if (p <= arr[num - 1])
+        {
+            if (arr[j] == p)
+            {
+
+                curr_top->next = cl_push(curr_top, 1);
+                j++;
+            }
+            else
+            {
+                temp = curr_top -> next;
+                min = sum;
+                pre = 0;
+                l = 0;
+                for (i = 0; arr[i] < p; i++)
+                {
+                    if (i > 0)
+                        pre = arr[i] - arr[i - 1];
+                    else 
+                        pre = arr[i];
+
+                    temp = list_skip(temp, pre);
+                    if ((temp->data < min) && (temp->data != 0))
+                    {
+                        min = temp->data;
+                        l = 1;
+                    }
+                }
+                if ((i == 0) || (l == 0))
+                    min = -1;
+                curr_top->next = cl_push(curr_top, min + 1);
+            }
+        }
+        else
+        {
+
+            min = sum;
+            pre = 0;
+            l = 0;
+            for (i = 0; i < num; i++)
+            {
+                if (i > 0)
+                    pre = arr[i] - arr[i - 1];
+                else 
+                    pre = arr[i];
+                temp = list_skip(temp, pre);
+                if ((temp->data < min) && (temp->data != 0))
+                {
+                    min = temp->data;
+                    l = 1;
+                }
+            }
+            if ((i == 0) || (l == 0))
+                min = -1;
+
+            temp = list_skip(curr_top, arr[num - 1]);
+            curr_top = cl_pop(temp);
+
+            curr_top->next = cl_push(curr_top, min + 1);
+
+            
+        }
+
+        //display_list(curr_top, arr[num - 1]);
+    }
+    //display_list(curr_top, p);
+    //delete_list(curr_top, arr[num - 1]);
+    return curr_top->next->data;
+}
+
+int comp_int(int *val1, int *val2)
+{
+    return (*val1 - *val2);
+}
+
+void arr_sort(int *arr, int n)
+{
+    qsort(arr, n, sizeof(int), (int *)comp_int);
+}
+
+void delete_list(struct list_t *top, int n)
+{
+    int i;
+    struct list_t *curr, *temp;
+    curr = top;
+    for(i = 0; i < n; i++)
+    {
+        temp = curr->next;
+        free(curr);
+        curr = temp;
+    }
+}
+
+void display_list(struct list_t *top, int n)
+{
+    struct list_t *curr;
+    int i;
+    curr = top->next;
+    printf("LIST:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("i = %d: %d on %p\n", n - i - 1, curr->data, curr);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int M, num, i;
+    int *arr;
+
+    scanf("%d%d", &M, &num);
+
+    arr = calloc(num, sizeof(int));
+    assert(arr != NULL);
+
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    qsort(arr, num, sizeof(int), (int *)comp_int);
+
+    /*
+    for (i = 0; i < num; i++)
+        printf("%d ", arr[i]);
+        
+    
+
+    printf("\n"); */
+
+    printf("%d\n", coin_num(arr, num, M));
+    
+}
---
+++ sem_5/tt.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+typedef struct node_t
+{
+    struct node_t *left;
+    struct node_t *right;
+    int data;
+} node;
+
+void to_read(int n, int *arr)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+}
+
+int arr_seek(int *arr, int n, int num)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == num)
+            return i;
+    }
+    return -1;
+}
+
+node *bin_tree_build(int nodes, int *in, int *pre)
+{
+    node *root;
+    int in_pos, pre_pos;
+
+    root = calloc(1, sizeof(node));
+    assert(root != NULL);
+    root->data = *pre;
+
+    if (((in_pos = arr_seek(in, nodes, pre[0])) != -1) && (in_pos != 0))
+    { //left node
+        root->left = bin_tree_build(in_pos, in, pre + 1);
+    }
+    else
+        root->left = NULL;
+
+    if (((nodes > in_pos + 1) && (in_pos != -1)) && (in_pos != nodes - 1)) //right node //in_pos != 0 ???
+        root->right = bin_tree_build(nodes - in_pos - 1, in + in_pos + 1, pre + in_pos + 1);
+    else
+        root->right = NULL;
+
+    //printf("Created node: %d addr: %p, L: %p, R: %p\n", root -> data, root, root -> left, root -> right);
+
+    return root;
+}
+
+int tree_topology_build(node *top, int *arr_topology, int *arr_data, int i)
+{
+
+    arr_topology[0] = 1;
+    arr_data[0] = top->data;
+
+    if (top->left != NULL)
+    {
+        
+        i = 1;
+        printf("Node: %d, left: %p, i = %d\n", top -> data , top -> left, i);
+        i = tree_topology_build(top->left, arr_topology + 1, arr_data + 1, i) + i;
+    }
+
+    if (top->right != NULL)
+    {
+        printf("Node: %d, right: %p, i = %d\n", top -> data , top -> right, i);
+        i = tree_topology_build(top->right, arr_topology + 2 * i, arr_data + i + 1, 0);
+        
+    }
+
+    return i;
+}
+
+void tree_topology_print(node *top, int n)
+{
+    int *arr_topology;
+    int *arr_data;
+    int i;
+    arr_topology = calloc(2 * n, sizeof(int));
+    arr_data = calloc(n, sizeof(int));
+
+    tree_topology_build(top, arr_topology, arr_data, 0);
+
+    printf("%d ", n);
+    printf("\n");
+    for (i = 0; i < 2 * n; i++)
+    {
+        printf("%d ", arr_topology[i]);
+    }
+    printf("\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr_data[i]);
+    }
+
+    printf("\n");
+}
+
+int main(void)
+{
+    int n, *preorder, *inorder, i;
+    node *top;
+    scanf("%d", &n);
+
+    preorder = calloc(n, sizeof(int));
+    inorder = calloc(n, sizeof(int));
+
+    to_read(n, preorder);
+    to_read(n, inorder);
+
+    top = bin_tree_build(n, inorder, preorder);
+    tree_topology_print(top, n);
+    return 0;
+}
